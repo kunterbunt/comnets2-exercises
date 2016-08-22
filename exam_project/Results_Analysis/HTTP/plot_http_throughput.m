356cd9d263dc09cb851518a19084363fea8c0c09
kunterbunt <to-kunterbunt@gmx.net>
2016-08-22 12:02:19 +0200

    http traffic converted to MB

--- exam_project/Results_Analysis/HTTP/plot_http_throughput.m
+++ exam_project/Results_Analysis/HTTP/plot_http_throughput.m
@@ -1,10 +1,14 @@
 function plot_http_throughput()
-    filenames = {'new-two-ThirtyfiveStudentsNOCCTV-rcvdPkSumBytes-1.csv', 'new-two-ThirtyfiveStudents-rcvdPkSumBytes-1.csv', 'new-two-FourtyfiveStudentsNOCCTV-rcvdPkSumBytes-1.csv', 'new-two-FortyfiveStudents-rcvdPkSumBytes-1.csv', 'new-two-FiftyfiveStudentsNOCCTV-rcvdPkSumBytes-1.csv', 'new-two-FiftyfiveStudents-rcvdPkSumBytes-1.csv', 'new-two-SixtyfiveStudentsNOCCTV-rcvdPkSumBytes-1.csv', 'new-two-SixtyiveStudents-rcvdPkSumBytes-1.csv'};    
+    filenames = {'../3rd_analysis/new-two-ThirtyfiveStudentsNOCCTV-rcvdPkSumBytes-1.csv', '../3rd_analysis/new-two-ThirtyfiveStudents-rcvdPkSumBytes-1.csv', '../3rd_analysis/new-two-FourtyfiveStudentsNOCCTV-rcvdPkSumBytes-1.csv', '../3rd_analysis/new-two-FortyfiveStudents-rcvdPkSumBytes-1.csv', '../3rd_analysis/new-two-FiftyfiveStudentsNOCCTV-rcvdPkSumBytes-1.csv', '../3rd_analysis/new-two-FiftyfiveStudents-rcvdPkSumBytes-1.csv', '../3rd_analysis/new-two-SixtyfiveStudentsNOCCTV-rcvdPkSumBytes-1.csv', '../3rd_analysis/new-two-SixtyiveStudents-rcvdPkSumBytes-1.csv'};    
     numbersOfStudents = [35 35 45 45 55 55 65 65];
     meansVec = [];
     CIsVec = [];
     for i=1:length(filenames)        
         [fileMean, fileCI] = getMeanFromCSV(filenames{1, i}, numbersOfStudents(i));
+        fileMean = fileMean./1000; % kB        
+        fileCI = fileCI./1000;
+        fileMean = fileMean./1000; % MB
+        fileCI = fileCI./1000;
         CIsVec = [CIsVec; fileCI];
         meansVec = [meansVec; fileMean];
     end
@@ -14,7 +18,7 @@ function plot_http_throughput()
     bar(meansVec)
     %errorbar(1:1:length(filenames), meansVec, CIsVec(1:end, 1), CIsVec(1:end, 2), 'rx')    
     errorbar(1:1:length(filenames), meansVec, CIsVec(1:end, 2) - CIsVec(1:end, 1), 'rx')    
-    ylabel('Total Download Traffic [Byte]');
+    ylabel('Total Download Traffic [MB]');
     xlabel('Number of students');
     set(gca,'XTickLabel',{'', '35 no CCTV', '35', '45 no CCTV', '45', '55 no CCTV', '55', '65 no CCTV', '65'})
     set(gca,'XTickLabelRotation', 90)
